0984fa49db0bf755cf62014c97f6f9b41d066c31
taihuynhz <129383417+taihuynhz@users.noreply.github.com>
2023-04-23 15:17:12 +0700

    Index Out Of Range Exception

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexOutOfRangeException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<char> characters = new List<char>();
+                characters.InsertRange(0, new Char[] { 'a', 'b', 'c', 'd', 'e', 'f' });
+                int value = characters[6];
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                System.Console.WriteLine("Index was out of range\n" + ex);
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine("Something was wrong\n" + ex);
+            }
+            finally
+            {
+                System.Console.WriteLine("Try again");
+            }
+        }
+    }
+}
